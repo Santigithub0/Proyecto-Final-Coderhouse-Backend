82b5f23fb651bf5fb701f6912e34282ac33563bd
¨Santiago <santiagogrela@gmail.com>
2023-03-11 17:55:31 -0300

    Handlebars y websocket funcionando

--- src/public/js/script.js
+++ src/public/js/script.js
@@ -1,5 +1,5 @@
 const socket = io();
-const cardsContainer = document.getElementById("cards-container");
+const cardsContainer = document.getElementById("cards");
 
 const title = document.getElementById("title");
 const description = document.getElementById("description");
@@ -9,25 +9,25 @@ const category = document.getElementById("category");
 const price = document.getElementById("price");
 const submit = document.getElementById("submit");
 
-const isEmpty = (...args) => args.some((arg) => !arg);
+const clear = (...prods) => prods.some((prod) => !prod);
 
 const deleteProduct = (id) => {
   socket.emit("deleteProduct", id);
 };
 
-const resetForm = () => { 
+const resetForm = () => {
   title.value = ""; description.value = ""; code.value = ""; stock.value = ""; category.value = ""; price.value = ""; files.value = "";
 };
 
-const render = (data) => {  
+const render = (data) => {
   cardsContainer.innerHTML = "";
   data.forEach((product) => {
     cardsContainer.innerHTML += `
-      <div class="d-flex flex-column align-items-start justify-content-between rounded align-text-bottom border border-bottom-0 border-end-0 border-start-0 border-secondary p-3 mb-3 bg-light" style="width: 18rem;">
-        <h5>${product.title}</h5>
-        <p>${product.price}</p>
-        <p>${product.description}</p>
-        <button class="btn btn-danger" onclick="deleteProduct('${product.id}')" style="--bs-btn-padding-y: .25rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem;">
+      <div class="d-flex flex-column align-items-start justify-content-between align-text-bottom border border-bottom-0 border-end-0 border-start-0 border-secondary p-3 mb-3 bg-dark cards">
+        <h5 class="text-light">${product.title}</h5>
+        <p class="text-light">${product.description}</p>
+        <p class="text-light">${product.price}</p>
+        <button class="btn btn-danger" onclick="deleteProduct('${product.id}')">
           Eliminar
         </button>
       </div>
@@ -38,11 +38,21 @@ const render = (data) => {
 submit.addEventListener("click", (e) => {
   e.preventDefault();
 
-  if (isEmpty(title.value, description.value, code.value, stock.value, category.value, price.value)) {
-    return alert("All fields are required.");
+  if (clear(title.value, description.value, code.value, stock.value, category.value, price.value)) {
+    Swal.fire(
+      'Oops..!',
+      'All fields are required!',
+      'error'
+    );
   } else {
     const newProduct = { title: title.value, description: description.value, code: code.value, stock: stock.value, category: category.value, price: price.value };
     socket.emit("addProduct", newProduct);
+
+    Swal.fire(
+      'Great!',
+      'Product added!',
+      'success'
+    );
 
     resetForm();
   }
