8e7db7141d8baa9e138b49fa85100849992c8ec1
¨Santiago <santiagogrela@gmail.com>
2023-03-12 19:36:40 -0300

    Correcciones

--- src/socket/socketConfiguration.js
+++ src/socket/socketConfiguration.js
@@ -8,30 +8,19 @@ const socketConfiguraton = (httpServer) => {
 
     io.on("connection", async (socket) => {
         console.log(`user connected: ${socket.id}`);
-
         const products = await filemanager.getProducts();
-
         socket.emit("newProduct", products);
-
         socket.on("addProduct", async (product) => {
-
             await filemanager.addProducts(product);
-
             const products = await filemanager.getProducts();
-
             io.sockets.emit("newProduct", products);
-
             socket.broadcast.emit("newProduct", products);
         });
 
         socket.on("deleteProduct", async (id) => {
-
             await filemanager.deleteProducts(id);
-
             const products = await filemanager.getProducts();
-
             io.sockets.emit("newProduct", products);
-
             socket.broadcast.emit("newProduct", products);
         });
     });
